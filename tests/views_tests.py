3bf61d0c90c2569136443175317428acc6c62206
Kenneth Lebu <kenlebu@gmail.com>
2017-09-21 13:05:31 +0300

    Add tests for views

---
+++ tests/views_tests.py
@@ -0,0 +1,33 @@
+import unittest
+import app.views
+from app.user import User
+from flask import url_for
+
+class ViewsTests(unittest.TestCase):
+    """ Tests for the views """
+
+    view = app.views
+
+    def test_signup(self):
+        """ Tests whether signup page loads """
+
+        with app.app.test_client() as client:
+            response = client.get('/signup')
+            self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        """ Tests whether login page is loaded """
+
+        with app.app.test_client() as client:
+            response = client.get('/login')
+            self.assertEqual(response.status_code, 200)
+
+    def test_index(self):
+        """ Tests whether index page loads """
+
+        with app.app.test_client() as client:
+            response = client.get('/')
+            self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
